b56a6e8481213cf73360d89c875ee7124b216375
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-05-12 14:55:23 -0700

    import rxjs/add/operator/toPromise in git-search.service

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { GitSearch } from './git-search'
 import { promise } from 'protractor';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable({
   providedIn: 'root'
