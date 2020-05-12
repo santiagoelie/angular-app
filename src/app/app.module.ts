dd0b97c18bf0881e159f80cf1fb86e9568897e67
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-05-12 15:13:48 -0700

    import GitSearchService into app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { GitSearchService } from './git-search.service';
  
 @NgModule({
   declarations: [
