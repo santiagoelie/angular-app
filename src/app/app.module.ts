b2f792eaf08fd1db6d036f601028cdc9ce9e7172
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-05-13 13:28:46 -0700

    modify some changes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GitSearchService } from './git-search.service';
+import { GitSearchComponent } from './git-search/git-search.component';
  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitSearchComponent
   ],
   imports: [
     BrowserModule,
